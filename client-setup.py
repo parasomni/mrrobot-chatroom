9dab51a7b72dac039306a98cfecfc1b0e9570671
rysecx <98301912+rysecx@users.noreply.github.com>
2023-06-07 00:14:07 +0200

    Update client-setup.py

--- client-setup.py
+++ client-setup.py
@@ -1,17 +1,7 @@
 #!/bin/python3
 import os 
 
-def check_dir(dirPath):
-    if os.path.exists(str(dirPath)):
-        #self.print_log(f'Directory {dirPath} exists.')
-        pass
-    else:
-        print(f'Directory {dirPath} does not exist --> creating...')
-        os.makedirs(dirPath)
-
 os.system("cp join.py /usr/bin/join")
 os.system("chmod +x /usr/bin/join")
 check_dir("/etc/join")
 print("setting up triggers")
-os.system("cp user.txt /etc/join/")
-os.system("cp key.txt /etc/join/key-chatroom.txt")
