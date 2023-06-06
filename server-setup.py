7f15e56416ae157675beaa29371baef713f3e95d
rysecx <98301912+rysecx@users.noreply.github.com>
2023-06-07 00:07:18 +0200

    Fixed server-setup.py

--- server-setup.py
+++ server-setup.py
@@ -1,14 +1,14 @@
 #!/bin/python3
 
 import os 
-from cryptography import fernet
+from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
 with open("key.txt", "wb") as f:
     f.write(key)
 f.close()
 print("private key written to key.txt")
-os.system('cp mrrobot-server.py /usr/bin/mrrobot-server')
+os.system('cp server.py /usr/bin/mrrobot-server')
 os.system('chmod +x /usr/bin/mrrobot-server')
 print("setting up triggers")
 os.system('echo ",<elliot>," > validUsers.txt')
